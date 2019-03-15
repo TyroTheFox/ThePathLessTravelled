2271175c07b3c72225593c0bf0f0c14816645b5c
Twin Tailed Fox <boyinthreeparts@gmail.com>
2019-03-15 02:34:01 +0000

    Building Forrest Areas, Adding Camera Zones and ORK Elements

--- Assets/Utilities/Rewired/Extras/ControlMapper/Scripts/CustomToggle.cs
+++ Assets/Utilities/Rewired/Extras/ControlMapper/Scripts/CustomToggle.cs
@@ -1,7 +1,4 @@
 ﻿// Copyright (c) 2015 Augie R. Maddox, Guavaman Enterprises. All rights reserved.
-
-using System.Linq;
-
 #pragma warning disable 0219
 #pragma warning disable 0618
 #pragma warning disable 0649
@@ -60,28 +57,28 @@ namespace Rewired.UI.ControlMapper {
 
         public override Selectable FindSelectableOnLeft() {
             if((navigation.mode & Navigation.Mode.Horizontal) != 0 || _autoNavLeft) {
-                return UISelectionUtility.FindNextSelectable(this, transform, Selectable.allSelectables.ToList(), Vector3.left);
+                return UISelectionUtility.FindNextSelectable(this, transform, Selectable.allSelectables, Vector3.left);
             }
             return base.FindSelectableOnLeft();
         }
 
         public override Selectable FindSelectableOnRight() {
             if((navigation.mode & Navigation.Mode.Horizontal) != 0 || _autoNavRight) {
-                return Rewired.UI.ControlMapper.UISelectionUtility.FindNextSelectable(this, transform, Selectable.allSelectables.ToList(), Vector3.right);
+                return Rewired.UI.ControlMapper.UISelectionUtility.FindNextSelectable(this, transform, Selectable.allSelectables, Vector3.right);
             }
             return base.FindSelectableOnRight();
         }
 
         public override Selectable FindSelectableOnUp() {
             if((navigation.mode & Navigation.Mode.Vertical) != 0 || _autoNavUp) {
-                return Rewired.UI.ControlMapper.UISelectionUtility.FindNextSelectable(this, transform, Selectable.allSelectables.ToList(), Vector3.up);
+                return Rewired.UI.ControlMapper.UISelectionUtility.FindNextSelectable(this, transform, Selectable.allSelectables, Vector3.up);
             }
             return base.FindSelectableOnUp();
         }
 
         public override Selectable FindSelectableOnDown() {
             if((navigation.mode & Navigation.Mode.Vertical) != 0 || _autoNavDown) {
-                return Rewired.UI.ControlMapper.UISelectionUtility.FindNextSelectable(this, transform, Selectable.allSelectables.ToList(), Vector3.down);
+                return Rewired.UI.ControlMapper.UISelectionUtility.FindNextSelectable(this, transform, Selectable.allSelectables, Vector3.down);
             }
             return base.FindSelectableOnDown();
         }
